0114e33720b2fc3793cdec22e71e82385eb06a50
mnallamalli97 <krish@magnabid.com>
2019-05-22 02:02:37 -0700

    connected to backend

--- H0tlin3bling/app/src/main/java/com/example/mnallamalli97/h0tlin3bling/MainActivity.java
+++ H0tlin3bling/app/src/main/java/com/example/mnallamalli97/h0tlin3bling/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.mnallamalli97.h0tlin3bling;
 
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,14 +13,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "TAG";
 
 
     Button btnSubmit;
@@ -25,26 +43,129 @@ public class MainActivity extends AppCompatActivity {
     RadioButton orBtn;
     RadioButton andBtn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         btnSubmit = findViewById(R.id.btnSubmit);
         etSearch = findViewById(R.id.etSearch);
         orBtn = findViewById(R.id.rbOr);
         andBtn = findViewById(R.id.rbAnd);
 
-        //AWS connection code
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
-            @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
-            }
-        }).execute();
 
 
+        String search_query = etSearch.getText().toString();
 
+        //set the default press to and
+        andBtn.setChecked(true);
+
+        Log.d(TAG,"doInBackground: starting parsetask class");
+        new ParseTask().execute();
+
+        //if radio button = AND
+        if (andBtn.isEnabled()){
+
+        }
+
+        if (orBtn.isEnabled()){
+
+        }
 
     }
+
+    private HashMap<String, String> createSongItem(String name, String aname){
+        HashMap<String, String> songObject = new HashMap<String, String>();
+        songObject.put(name, aname);
+        return songObject;
+    }
+
+
+    private class ParseTask extends AsyncTask<Void, Void, String>{
+
+        //connect to the backend with the url
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String $url_json;
+        URL url;
+
+
+
+        ArrayList<HashMap<String,String>> employeeList = new ArrayList<HashMap<String,String>>();
+
+        String result_json = "{\"searchResults\":";
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                Log.d(TAG, "doInBackground: starting doinbackground");
+                $url_json = "https://lit-dusk-32149.herokuapp.com/search/and/i%20need%20a%20hero";
+                url = new URL($url_json);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                Log.d(TAG, "doInBackground: connecting to heroku");
+
+                //convert the JSON into a string
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = reader.readLine()) != null){
+                    buffer.append(line);
+                }
+                result_json = result_json.concat(buffer.toString());
+                result_json = result_json.concat("}");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result_json;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            super.onPostExecute(json);
+
+            //parse the string and put it into the list view
+            JSONObject jsonResponse = null;
+            try {
+
+                Log.d(TAG, result_json);
+               // String jsonString = "{\"searchResults\":[{\"sname\":\"I Wanna Be Your Hero\",\"aname\":\"Def Leppard\",\"url\":\"/d/def+leppard/i+wanna+be+your+hero_20038946.html\"},{\"sname\":\"Hero\",\"aname\":\"David Guetta\",\"url\":\"/d/david+guetta/hero_20991447.html\"},{\"sname\":\"Nobody's Hero\",\"aname\":\"Bon Jovi\",\"url\":\"/b/bon+jovi/nobodys+hero_20811564.html\"},{\"sname\":\"We Don't Need Another Hero\",\"aname\":\"Tina Turner\",\"url\":\"/t/tina+turner/we+dont+need+another+hero_10232469.html\"},{\"sname\":\"Hero For A Day\",\"aname\":\"Conway Twitty\",\"url\":\"/c/conway+twitty/hero+for+a+day_20870543.html\"}]}";
+                jsonResponse = new JSONObject(result_json);
+                JSONArray jsonMainNode = jsonResponse.optJSONArray("searchResults");
+
+                for(int i = 0; i<jsonMainNode.length();i++){
+                    JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                    String sname = jsonChildNode.optString("sname");
+                    String aname = jsonChildNode.optString("aname");
+                    String url = jsonChildNode.optString("url");
+                    String output = sname + "-" + aname;
+                    employeeList.add(createSongItem("result", output));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            ListView lvResults = findViewById(R.id.lvResults);
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(MainActivity.this, employeeList, android.R.layout.simple_list_item_1, new String[] {"result"}, new int[] {android.R.id.text1});
+            Log.d(TAG, "onPostExecute:" + employeeList);
+            lvResults.setAdapter(simpleAdapter);
+
+            result_json = "";
+
+        }
+    }
+
 }
